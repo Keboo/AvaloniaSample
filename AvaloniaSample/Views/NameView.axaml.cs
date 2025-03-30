943c371aea69966bef993a3c611395445c13c22d
Kevin Bost <kitokeboo@gmail.com>
2025-03-30 14:22:01 -0700

    Tabs with view model and view separation

--- AvaloniaSample/Views/NameView.axaml.cs
+++ AvaloniaSample/Views/NameView.axaml.cs
@@ -1,8 +1,6 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
-namespace AvaloniaSample;
+namespace AvaloniaSample.Views;
 
 public partial class NameView : UserControl
 {
